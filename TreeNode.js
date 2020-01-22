e14bf203660254ac32045f1728e24590ccbbf5a7
Jose <jogarcia@tuta.io>
2020-01-22 10:26:01 -0500

    Remove the unuseful brothe stuff

--- TreeNode.js
+++ TreeNode.js
@@ -2,7 +2,6 @@ class TreeNode {
   constructor(value, parent) {
     this.value = value;
     this.parent = parent;
-    this.brother = null;
     this.children = [];
   }
 
@@ -12,20 +11,6 @@ class TreeNode {
     }
 
     this.children.push(treeNodeChildren);
-    return 0;
-  }
-
-  addBrother(treeNodeChildren) {
-    if (!(treeNodeChildren instanceof TreeNode)) {
-      return 1;
-    }
-
-    let currentBro = this.brother;
-    while (currentBro) {
-      currentBro = currentBro.getBrother();
-    }
-
-    currentBro.brother = treeNodeChildren;
     return 0;
   }
 
@@ -41,37 +26,8 @@ class TreeNode {
     return 0;
   }
 
-  removeBrother(index) {
-    if (!(index instanceof Number)) {
-      return 1;
-    }
-
-    let currentBro = this.brother;
-    let beforeBro = null;
-    let i = 0;
-    while (currentBro && i != index) {
-      beforeBro = currentBro;
-      currentBro = currentBro.getBrother();
-    }
-
-    if (i != index) {
-      return -1;
-    }
-
-    if (!currentBro) {
-      beforeBro = null;
-    } else {
-      beforeBro = currentBro.getBrother();
-    }
-
-    return 0;
-  }
-
   getChildren() {
     return this.children;
   }
 
-  getBrother() {
-    return this.brother;
-  }
 }
