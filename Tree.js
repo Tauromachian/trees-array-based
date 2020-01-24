15349a8dfb136416cc9795f0ced7f076b69c262d
Jose <jogarcia@tuta.io>
2020-01-24 14:37:41 -0500

    Take out addChildrenNode method

--- Tree.js
+++ Tree.js
@@ -10,54 +10,6 @@ class Tree {
   }
 
   /**
-   * Searchs in the tree hierarchy and adds a new node in its corresponing place
-   * Inserts a node to the end of the name hierarchy
-   * @param  {String} nodeName The name of the node thats gonna be inserted
-   */
-  addChildrenToNodeByName(nodeName) {
-    if (!Validator.isString(nodeName)) {
-      return -1;
-    }
-
-    if (nodeName.split(".").length <= 1) {
-      this.head.addChildren(new TreeNode(nodeName, this.head));
-      return this.head;
-    }
-
-    let node = this.searchNodeByName(nodeName);
-    node.addChildren(new TreeNode(nodeName, node));
-    return node;
-  }
-
-  /**
-   * Search a the parent node and insert the children
-   * @param  {String} nodeName name of the node to insert
-   * @param  {String} node parent of the node that will be inserted
-   */
-  addChildrenNode(nodeName, nodeParent) {
-    if (!Validator.isString(nodeName)) {
-      return -1;
-    }
-
-    let newTreeNode = new TreeNode(nodeName);
-
-    if (!nodeParent) {
-      newTreeNode.putParent(this.head);
-      this.head.addChildren(newTreeNode);
-      return newTreeNode;
-    }
-
-    if (!Validator.isString(nodeParent)) {
-      return -1;
-    }
-
-    let parentNode = this.searchNodeByName(nodeParent);
-    newTreeNode.putParent(parentNode);
-    parentNode.addChildren(newTreeNode);
-    return newTreeNode;
-  }
-
-  /**
    * Searchs in the tree a node with the name providen
    * @param  {String} nodeName name of the node to search
    */
@@ -79,7 +31,7 @@ class Tree {
         return this._recursiveSearch(nodeChildren, nodeName);
       }
     }
-}
+  }
 
   isEqual(nodeName, node) {
     if (node.name === nodeName) {
