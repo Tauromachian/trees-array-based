04e1512f15b56c3516482d1d30c99a4e7793e7c8
Jose <jogarcia@tuta.io>
2020-01-27 10:21:52 -0500

    Create addChildrenMethod

--- Tree.js
+++ Tree.js
@@ -14,19 +14,26 @@ class Tree {
     }
     if (object instanceof String) {
       let node = new TreeNode(object, nodeParent);
-      this.addChildrenNode(object, nodeParent);
+      this.addChildrenNode(node, nodeParent);
     }
     return 0;
   }
 
-  addChildrenNode(treeNode, nodeParent){
-
+  addChildrenNode(treeNode, nodeParent) {
+    let parentNode = this._wideSearch(this.head.children, nodeParent, []);
+    parentNode.addChildren(treeNode);
   }
 
-  
+  _wideSearch(children, node, nodeArray) {
+    for (const i in children) {
+      if(this.isEqual(children[key], node)){
+        nodeArray.push(children[key]);
+      }
+    }
+  }
 
-  isEqual(nodeName, node) {
-    if (node.name === nodeName) {
+  isEqual(nodeOne, nodeTwo) {
+    if (nodeOne.value === nodeTwo.value) {
       return 1;
     }
     return 0;
