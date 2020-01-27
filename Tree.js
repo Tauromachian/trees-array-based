9931a0316502915824cf0a25b1f8d89c71941877
Jose <jogarcia@tuta.io>
2020-01-27 10:01:11 -0500

    Take out search by name

--- Tree.js
+++ Tree.js
@@ -8,29 +8,22 @@ class Tree {
     this.head = new TreeNode(null);
   }
 
-  /**
-   * Searchs in the tree a node with the name providen
-   * @param  {String} nodeName name of the node to search
-   */
-  searchNodeByName(nodeName) {
-    let node = this._recursiveSearch(this.head.children, nodeName);
-    if (node) {
-      return node;
-    } else return 0;
-  }
-
-  _recursiveSearch(nodeChildren, nodeName) {
-    for (const key in nodeChildren) {
-      if (nodeChildren[key].name === nodeName) {
-        return nodeChildren[key];
-      }
-
-      if (nodeName.includes(nodeChildren[key].name)) {
-        nodeChildren = node.nodeChildren;
-        return this._recursiveSearch(nodeChildren, nodeName);
-      }
+  addChildren(object, nodeParent) {
+    if (object instanceof TreeNode) {
+      this.addChildrenNode(object, nodeParent);
     }
+    if (object instanceof String) {
+      let node = new TreeNode(object, nodeParent);
+      this.addChildrenNode(object, nodeParent);
+    }
+    return 0;
   }
+
+  addChildrenNode(treeNode, nodeParent){
+
+  }
+
+  
 
   isEqual(nodeName, node) {
     if (node.name === nodeName) {
