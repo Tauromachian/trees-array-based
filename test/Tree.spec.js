0d656f78ec7478fc4f915cdd0c5160f89027bb95
jogarcia <jogarcia@uclv.cu>
2020-03-06 21:39:14 -0500

    Fix addChildren test for return number

--- test/Tree.spec.js
+++ test/Tree.spec.js
@@ -3,6 +3,7 @@ const expect = chai.expect;
 const should = chai.should();
 
 const Tree = require("./../Tree");
+const TreeNode = require("./../TreeNode");
 
 describe("Tree.js", function () {
   it("Should exist", function () {
@@ -12,7 +13,8 @@ describe("Tree.js", function () {
   describe("Trees addChildren", function () {
     it("Should return a number", function () {
       let tree = new Tree();
-      tree.addChildren.should.be.a("number");
+      let treeNode = new TreeNode("lol");
+      tree.addChildren(treeNode).should.be.a("number");
     }); 
   });
 });
