b75518def19d22d8028e654ceb1bf624527d0c33
jogarcia <jogarcia@uclv.cu>
2020-03-07 14:02:15 -0500

    Describe getChild

--- test/TreeNode.spec.js
+++ test/TreeNode.spec.js
@@ -19,12 +19,12 @@ describe("TreeNode.js", function() {
       let parentNode = new TreeNode("parent");
       parentNode.addChildren(null).should.equal(1);
     });
-    it("Should add the parent node to the child'is parent property",function () {
+    it("Should add the parent node to the child'is parent property", function() {
       let parentNode = new TreeNode("parent");
       let childNode = new TreeNode("child");
       parentNode.addChildren(childNode);
       expect(childNode.parent).to.equal(parentNode);
-    })
+    });
     it("Should insert correctly in the array of the node parent", function() {
       let parentNode = new TreeNode("parent");
       let parentNode2 = parentNode;
@@ -46,48 +46,48 @@ describe("TreeNode.js", function() {
     });
   });
 
-  describe("getChildren", function () {
-    it("Should exist", function () {
+  describe("getChildren", function() {
+    it("Should exist", function() {
       let parentNode = new TreeNode("parent");
       expect(parentNode.getChildren).to.exist;
     });
-    it("Should return array", function () {
+    it("Should return array", function() {
       let parentNode = new TreeNode("parent");
       parentNode.getChildren().should.be.an("array");
     });
-    it("Should return empty array", function () {
+    it("Should return empty array", function() {
       let parentNode = new TreeNode("parent");
       parentNode.getChildren("child").should.be.an("array");
     });
-    it("Should return array", function () {
+    it("Should return array", function() {
       let parentNode = new TreeNode("parent");
       let childNode = new TreeNode("child");
       let childNode0 = new TreeNode("child");
       let childNode2 = new TreeNode("child2");
       parentNode.addChildren(childNode);
       parentNode.addChildren(childNode2);
-      
+
       parentNode.getChildren("child").should.be.an("array");
     });
-    it("Should return all the node's children",function () {
+    it("Should return all the node's children", function() {
       let parentNode = new TreeNode("parent");
       let childNode = new TreeNode("child");
       let childNode2 = new TreeNode("child2");
       parentNode.addChildren(childNode);
       parentNode.addChildren(childNode2);
-      
+
       parentNode.getChildren().should.equal(parentNode.children);
     });
-    it("Should return the matching node children and no more",function () {
+    it("Should return the matching node children and no more", function() {
       let parentNode = new TreeNode("parent");
       let childNode = new TreeNode("child");
       let childNode2 = new TreeNode("child2");
       parentNode.addChildren(childNode);
       parentNode.addChildren(childNode2);
-      
+
       parentNode.getChildren("child").should.not.equal(parentNode.children);
     });
-    it("Should return the matching node children",function () {
+    it("Should return the matching node children", function() {
       let parentNode = new TreeNode("parent");
       let childNode = new TreeNode("child");
       let childNode2 = new TreeNode("child2");
@@ -97,7 +97,7 @@ describe("TreeNode.js", function() {
       let children = parentNode.getChildren("child");
       children[0].value.should.be.equal("child");
     });
-    it("Should return all the matching nodes children",function () {
+    it("Should return all the matching nodes children", function() {
       let parentNode = new TreeNode("parent");
       let childNode = new TreeNode("child");
       let childNode0 = new TreeNode("child");
@@ -105,11 +105,18 @@ describe("TreeNode.js", function() {
       parentNode.addChildren(childNode);
       parentNode.addChildren(childNode0);
       parentNode.addChildren(childNode2);
-      
+
       let children = parentNode.getChildren("child");
 
       children[0].value.should.be.equal("child");
       children[1].value.should.be.equal("child");
+    });
+  });
+
+  describe("getChildren", function() {
+    it("Should exist", function() {
+      const parentNode = new TreeNode("parent");
+      expect(parentNode.getChild).to.exist;
     });
   });
 });
