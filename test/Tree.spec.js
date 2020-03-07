1a99329c6ecc971ea107eba41c6518a1645a9e27
jogarcia <jogarcia@uclv.cu>
2020-03-07 10:16:30 -0500

    Test addChildren return type

--- test/Tree.spec.js
+++ test/Tree.spec.js
@@ -5,17 +5,25 @@ const should = chai.should();
 const Tree = require("./../Tree");
 const TreeNode = require("./../TreeNode");
 
-describe("Tree.js", function () {
-  it("Should exist", function () {
+describe("Tree.js", function() {
+  it("Should exist", function() {
     expect(Tree).to.exist;
   });
 
-  describe("Trees addChildren", function () {
-    it("Should return a number", function () {
+  describe("Trees addChildren", function() {
+    it("Should return a number", function() {
       let tree = new Tree();
-      let treeNode = new TreeNode("lol");
+      let treeNode = new TreeNode("testName");
       tree.addChildren(treeNode).should.be.a("number");
-    }); 
+    });
+    it("Should return a 1", function() {
+      let tree = new Tree();
+      tree.addChildren().should.equal(1);
+    });
+    it("Should return a 0", function() {
+      let tree = new Tree();
+      let treeNode = new TreeNode("testName");
+      tree.addChildren(treeNode).should.equal(0);
+    });
   });
 });
-
