2a56e4e5662a3291c45d38829f09f2d8add78bc0
jogarcia <jogarcia@uclv.cu>
2020-03-07 12:10:24 -0500

    Create exist test

--- test/TreeNode.spec.js
+++ test/TreeNode.spec.js
@@ -2,5 +2,10 @@ const chai = require("chai");
 const expect = chai.expect;
 const should = chai.should();
 
-const Tree = require("./../Tree");
 const TreeNode = require("./../TreeNode");
+
+describe("TreeNode.js", function () {
+  it("Should exist", function () {
+    expect(TreeNode).to.exist;
+  })
+})
