af2e756f768e1d9134a853c97a6e04e3d7d6d0d6
jogarcia <jogarcia@uclv.cu>
2020-03-07 12:14:02 -0500

    Make basic test to addChildren return type

--- test/TreeNode.spec.js
+++ test/TreeNode.spec.js
@@ -4,8 +4,16 @@ const should = chai.should();
 
 const TreeNode = require("./../TreeNode");
 
-describe("TreeNode.js", function () {
-  it("Should exist", function () {
+describe("TreeNode.js", function() {
+  it("Should exist", function() {
     expect(TreeNode).to.exist;
-  })
-})
+  });
+
+  describe("addChildren", function() {
+    it("Should return 0", function() {
+      let parentNode = new TreeNode("parent");
+      let childNode = new TreeNode("child");
+      parentNode.addChildren(childNode).should.equal(0);
+    });
+  });
+});
