917e4971bd911d3386100676e187473c3d11e23e
jogarcia <jogarcia@uclv.cu>
2020-03-12 15:02:18 -0400

    Check that exists

---
+++ test/NameHierarchicalTree.spec.js
@@ -0,0 +1,13 @@
+const chai = require("chai");
+const expect = chai.expect;
+const should = chai.should();
+
+const NameHierarchicalTree= require("./../NameHierarchicalTree");
+const TreeNode = require("./../TreeNode");
+
+describe("NameHierarchicalTree.js", function () {
+  it("Should exist", function () {
+    expect(NameHierarchicalTree).to.exist;
+  });
+});
+
