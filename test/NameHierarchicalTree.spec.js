9bc9db296a6b05f52cce4c22032d11110ad8d338
jogarcia <jogarcia@uclv.cu>
2020-03-12 15:08:10 -0400

    Check that addChild is inserting correctly

--- test/NameHierarchicalTree.spec.js
+++ test/NameHierarchicalTree.spec.js
@@ -2,12 +2,27 @@ const chai = require("chai");
 const expect = chai.expect;
 const should = chai.should();
 
-const NameHierarchicalTree= require("./../NameHierarchicalTree");
+const NameHierarchicalTree = require("./../NameHierarchicalTree");
 const TreeNode = require("./../TreeNode");
 
-describe("NameHierarchicalTree.js", function () {
-  it("Should exist", function () {
+describe("NameHierarchicalTree.js", function() {
+  it("Should exist", function() {
     expect(NameHierarchicalTree).to.exist;
   });
-});
 
+  describe("addChild", function() {
+    it("Should insert succesfully", function() {
+      let nameTree = new NameHierarchicalTree();
+
+      nameTree.addChild("parent");
+      nameTree.addChild("parent.child1");
+      nameTree.addChild("parent.child2");
+      nameTree.addChild("parent.child1.child1-1");
+
+      nameTree.head.children[0].value.should.equal("parent");
+      nameTree.head.children[0].children[0].value.should.equal("parent.child1");
+      nameTree.head.children[0].children[1].value.should.equal("parent.child2");
+      nameTree.head.children[0].children[0].children[0].value.should.equal("parent.child1.child1-1");
+    });
+  });
+});
