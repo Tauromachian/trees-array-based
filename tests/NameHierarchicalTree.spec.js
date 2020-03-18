3ba6380a0f5c5c8d9332b56a22331a40c905b912
jogarcia <jogarcia@uclv.cu>
2020-03-18 11:40:57 -0400

    Change dir name to tests

--- tests/NameHierarchicalTree.spec.js
+++ tests/NameHierarchicalTree.spec.js
@@ -2,8 +2,8 @@ const chai = require("chai");
 const expect = chai.expect;
 const should = chai.should();
 
-const NameHierarchicalTree = require("./../NameHierarchicalTree");
-const TreeNode = require("./../TreeNode");
+const NameHierarchicalTree = require("../NameHierarchicalTree");
+const TreeNode = require("../TreeNode");
 
 describe("NameHierarchicalTree.js", function() {
   it("Should exist", function() {
