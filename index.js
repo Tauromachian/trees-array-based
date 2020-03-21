b9e803c4ab60c85ff56fd5ba5084fb1f5e376a19
jogarcia <jogarcia@uclv.cu>
2020-03-21 09:23:14 -0400

    Install babel minify

---
+++ index.js
@@ -0,0 +1,7 @@
+const main = require("./dist/main").TreeNode;
+// const Tree = require("./Tree");
+// const NameHierarchicalTree = require("./NameHierarchicalTree");
+
+console.log(main);
+
+// const treeNode = new TreeNode();
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
+const MinifyPlugin = require("babel-minify-webpack-plugin");
+
 module.exports = {
   target: 'node',
   mode: 'production',
+  uglify: false,
+  minimize: true
 };
