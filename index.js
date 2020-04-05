e6bdd1c48a3b45975dbbec409da4db0e95ec4cdc
jogarcia <jogarcia@uclv.cu>
2020-04-05 18:22:16 -0400

    Remove unnecesary index file

--- index.js
+++
@@ -1,7 +0,0 @@
-const TreeNode = require("./src/TreeNode");
-const Tree = require("././src/Tree");
-const NameHierarchicalTree = require("././src/NameHierarchicalTree");
-
-console.log(main);
-
-// const treeNode = new TreeNode();
